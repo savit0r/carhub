043a2402398280efda0d665a2a1d7ceb9ce5b849
Rohit <rohitsatre33@gmail.com>
2024-10-29 21:06:02 +0530

    minor updates

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,26 @@
 "use client";
 
+import Image from 'next/image';
 import { useState } from 'react';
 import React from 'react'
 import SearchManufacturer from './SearchManufacturer';
+import { warnOptionHasBeenDeprecated } from 'next/dist/server/config';
+
+const SearchButton = ({ otherClasses} : 
+  { otherClasses : string})=>(
+  <button type = "submit" className={`-ml-3 z-10 
+  ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+      />
+      
+
+  </button>
+)
 
 const SearchBar = () => {
     const [manufacturer, setManufacturer]=useState ('');
@@ -15,8 +33,8 @@ const SearchBar = () => {
             <SearchManufacturer 
             manufacturer = {manufacturer}
             setManufacturer = {setManufacturer}
-            
             />
+            <SearchButton otherClasses="sm:hidden" />
         </div>
     </form>
   )
